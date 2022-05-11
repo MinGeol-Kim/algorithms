8882dfcfea3b2aa2b04cd54ece6082a051a27ce8
Ephian222 <rlaalsrjf6@naver.com>
2022-05-11 13:53:03 +0900

    병합정렬 추가

---
+++ mergesort.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void mergesort(int* arr, int left, int right);
+
+int main() {
+    int n;
+    cin >> n;
+    int* arr = new int[n];
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    mergesort(arr, 0, n-1);
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << endl;
+    }
+    delete arr;
+}
+
+void mergesort(int* arr, int left, int right) {
+    if(left < right) {
+        int mid = (right + left) / 2;
+        mergesort(arr, left, mid);
+        mergesort(arr, mid+1, right);
+        int* tmparr = new int[right+1];
+        int acnt = left, bcnt = mid + 1, tmpcnt = 0;
+        while(acnt <= mid && bcnt <= right) {
+            if(arr[acnt] < arr[bcnt]) {
+                tmparr[tmpcnt++] = arr[acnt++]; 
+            }
+            else if(arr[acnt] > arr[bcnt]) {
+                tmparr[tmpcnt++] = arr[bcnt++];
+            }
+        }
+        if(acnt == mid+1) {
+            while(bcnt <= right) {
+                tmparr[tmpcnt++] = arr[bcnt++];
+            }        
+        }
+        else if(bcnt == right+1) {
+            while(acnt <= mid) {
+                tmparr[tmpcnt++] = arr[acnt++];
+            }            
+        }
+        for(int i = left; i <= right; i++) {
+            arr[i] = tmparr[i - left];
+        }
+        delete tmparr;
+    }
+}
